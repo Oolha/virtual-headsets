8d99240353b4547f6a5c3ae6958e0441724ffcd6
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-12-11 01:39:07 +0100

    did footer for desktop

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -9,23 +9,43 @@ import { useState } from "react";
 
 const Footer = ({}) => {
   return (
-    <div className={css.footerBox}>
-      <div className={css.logoAndLinksBox}>
-        <Logo />
-        <SocialLinks />
-      </div>
-      <p className={css.phoneNumber}>(+994) 199-28-786</p>
-      <div className={css.navLinks}>
-        <div>
-          <h4 className={css.title}>Menu</h4>
-          <NavMenu />
+    <div className={css.footer}>
+      <div className={css.footerBox}>
+        <div className={css.logoAndLinksBox}>
+          <Logo />
+          <SocialLinks />
+          <p className={css.phoneNumber}>(+994) 199-28-786</p>
         </div>
-        <div>
-          <h4 className={css.title}>Company</h4>
-          <CompanyMenu />
+
+        <div className={css.navLinks}>
+          <div>
+            <h4 className={css.title}>Menu</h4>
+            <NavMenu />
+          </div>
+          <div>
+            <h4 className={css.title}>Company</h4>
+            <CompanyMenu />
+          </div>
         </div>
+        <Subscription />
       </div>
-      <Subscription />
+      <div className={css.footerEndBox}>
+        <picture className={css.linesPicture}>
+          <source
+            srcSet="../../../public/images/desktop/Line-footer-desktop.png"
+            media="(min-width: 1440px)"
+          />
+          <source
+            srcSet="../../../public/images/mobile/Line-footer-mobile.png"
+            media="(min-width: 320px)"
+          />
+          <img
+            src="../../../public/images/desktop/Line-footer-desktop.png"
+            alt="line"
+          />
+        </picture>
+        <p className={css.rights}>© 2025 HoloGaze. All rights reserved.</p>
+      </div>
     </div>
   );
 };
--- client/src/components/Subscription/Subscription.tsx
+++ client/src/components/Subscription/Subscription.tsx
@@ -7,27 +7,14 @@ const Subscription = ({}) => {
       <p className={css.text}>
         Sure, please provide your email address to subscribe to newsletter
       </p>
-      <input
-        type="text"
-        placeholder="Enter your mail..."
-        className={css.input}
-      />
-      <button className={css.btn}>Subscribe</button>
-      <picture className={css.linesPicture}>
-        <source
-          srcSet="../../../public/images/desktop/Line-footer-desktop.png"
-          media="(min-width: 1440px)"
+      <div className={css.inputAndBtnBox}>
+        <input
+          type="text"
+          placeholder="Enter your mail..."
+          className={css.input}
         />
-        <source
-          srcSet="../../../public/images/mobile/Line-footer-mobile.png"
-          media="(min-width: 320px)"
-        />
-        <img
-          src="../../../public/images/desktop/Line-footer-desktop.png"
-          alt="line"
-        />
-      </picture>
-      <p className={css.rights}>© 2025 HoloGaze. All rights reserved.</p>
+        <button className={css.btn}>Subscribe</button>
+      </div>
     </div>
   );
 };
