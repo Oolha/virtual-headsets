1c302fefe55bd852a22ba537bfd1acdb9f5ac1b7
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-12-26 18:23:57 +0100

    updated vr item

--- client/src/components/VROneHeadset/VROneHeadset.tsx
+++ client/src/components/VROneHeadset/VROneHeadset.tsx
@@ -9,10 +9,14 @@ export const VROneHeadset: React.FC<VROneHeadsetProps> = ({ item }) => {
   return (
     <div className={css.item}>
       <div className={css.vrItem}>
-        <h4 className={css.title}>{item.name}</h4>
-        <p className={css.text}>Compatibility: {item.compatibility}</p>
-        <p className={css.text}>Screen resolution: {item.screenResolution}</p>
-        <p className={css.price}>{item.price} $</p>
+        <div>
+          <h4 className={css.title}>{item.name}</h4>
+          <p className={css.text}>Compatibility: {item.compatibility}</p>
+
+          <p className={css.price}>{item.price} $</p>
+        </div>
+
+        <div className={css.box}></div>
       </div>
       <img src={item.photo} alt={item.name} className={css.img} />
     </div>
