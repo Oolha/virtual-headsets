2efb51ea788b02c32f1d2b0ab73c0082309925cf
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-01-21 21:19:01 +0100

    Update responsive styles for different screen sizes

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -5,11 +5,13 @@ import GamingCreator from "../../components/GamingCreator/GamingCreator";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 import TopGames from "../../components/TopGames/TopGames";
-import style from "./HomePage.module.css";
+import css from "./HomePage.module.css";
 
 const HomePage = ({}) => {
   return (
-    <div>
+    <div className={css.contentContainer}>
+      <div className={css.gradient}></div>
+      <div className={css.gradient1}></div>
       <Header />
       <Hero />
       <Features />
